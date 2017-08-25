262ae773e7074d2870c8ed261eec11bc17d1eec9
Sunny Bhadani <sunnybhadani7@gmail.com>
2017-08-25 20:10:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
  var gists = angular.module('gist', []);
-      gists.controller('gistCtrl', function ($scope, $http){
-        $http.get('https://api.github.com/gists/public?page=1&per_page=30').success(function(data) {
-          $scope.list = data;
-        });
-      });
+ gists.controller('gistCtrl', function($scope, $http) {
+     $http.get('https://api.github.com/gists/public?page=1&per_page=30').success(function(data) {
+         $scope.list = data;
+     });
+ });
