693af5749cb37f8bd6b1a0f71536ecbc5b0c4f58
Sunny Bhadani <sunnybhadani7@gmail.com>
2017-08-26 16:49:09 +0530

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
- var gists = angular.module('gist', ['angularUtils.directives.dirPagination']);
-      gists.controller('gistCtrl', function ($scope, $http){
-        $http.get('https://api.github.com/gists/public?per_page=100').success(function(data) {
-          $scope.list = data;
-        });
-      });
