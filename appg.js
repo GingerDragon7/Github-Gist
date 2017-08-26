ee3550955591ae6e32f91b88882bcfe89cc1cc80
Sunny Bhadani <sunnybhadani7@gmail.com>
2017-08-26 16:49:30 +0530

    Add files via upload

---
+++ appg.js
@@ -0,0 +1,6 @@
+ var gists = angular.module('gist', ['angularUtils.directives.dirPagination']);
+      gists.controller('gistCtrl', function ($scope, $http){
+        $http.get('https://api.github.com/gists/public?per_page=100').success(function(data) {
+          $scope.list = data;
+        });
+      });
